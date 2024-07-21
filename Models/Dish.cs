5fcac704dac4badc6d0e852b9d3813bd2ab3bab5
Ellie <ellieslife@icloud.com>
2024-07-21 15:12:51 -0400

    add dish class and restaurant

---
+++ Models/Dish.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantList.Models
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+    }
+}
---
+++ Models/Restaurant.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantList.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string Address { get; set; }
+
+    }
+}
