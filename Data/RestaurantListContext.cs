776df22da72d552cd6cc13462d613277aec7d619
Ellie <ellieslife@icloud.com>
2024-08-11 16:26:25 -0400

    add stuff

--- Data/RestaurantListContext.cs
+++ Data/RestaurantListContext.cs
@@ -27,7 +27,32 @@ namespace RestaurantList.Data
                 .WithMany(rd => rd.RestaurantDishes)
                 .HasForeignKey(d => d.DishId);
 
+            modelBuilder.Entity<Restaurant>().HasData(new Restaurant
+            {
+                Id = 1,
+                Name = "Gourmet Pizzaria",
+                Address = "1234 Culinary St, Flavor, CA 90210",
+                ImageUrl = "https://www.whereyoueat.com/r_gallery_images/rgallery-21635/Best_Italian_Pizza2.jpg"
+            }
+            );
+
+            modelBuilder.Entity<Dish>().HasData(
+                new Dish { Id = 1, Name = "Pizza", Price = 10 },
+                new Dish { Id = 2, Name = "Pasta", Price = 9 }
+
+            );
+
+            modelBuilder.Entity<RestaurantDish>().HasData(
+                new RestaurantDish { RestaurantId = 1, DishId = 1},
+                new RestaurantDish { RestaurantId = 1, DishId = 2}                                                                              
+            );
+
             base.OnModelCreating(modelBuilder);
         }
+
+        public DbSet<Restaurant> Restaurants { get; set;}
+        public DbSet<Dish> Dishes { get; set;}
+        public DbSet<RestaurantDish> RestaurantsDish { get; set;}
+
     }
 }
